b9de1a7d7cc7ceac24d2bd4fd87effb7fe8de955
sem1312 <108529990+sem1312@users.noreply.github.com>
2023-10-27 12:06:22 -0300

    Add files via upload

---
+++ ej3.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+
+  public static void evaluateRpn (){
+    
+    string tamaño0 = Console.ReadLine();
+    
+    int.TryParse(tamaño0, out int tamaño);
+
+    Queue<int> myQueue = new Queue<int>();
+
+    for(int i = 0; i < tamaño; i++){                   string num0 = Console.ReadLine();
+      int.TryParse(num0, out int num);
+      myQueue.Enqueue(num);
+    }
+
+    foreach(var item in myQueue){
+      if(item % 2 != 0){
+        Console.WriteLine(item);
+      }
+    }
+  }
+  
+  public static void Main (string[] args) {
+    evaluateRpn();
+  }
+}
