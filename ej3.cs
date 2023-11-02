9864c936eb7a2e88d8d2c9cafbefdcd1c1382891
sem1312 <108529990+sem1312@users.noreply.github.com>
2023-11-02 10:09:33 -0300

    Update ej3.cs

--- ej3.cs
+++ ej3.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 class Program {
 
   public static void evaluateRpn (){
-    
+
     string tamaño0 = Console.ReadLine();
-    
     int.TryParse(tamaño0, out int tamaño);
 
     Queue<int> myQueue = new Queue<int>();
 
-    for(int i = 0; i < tamaño; i++){                   string num0 = Console.ReadLine();
+    for(int i = 0; i < tamaño; i++){                 
+      string num0 = Console.ReadLine();
       int.TryParse(num0, out int num);
       myQueue.Enqueue(num);
     }
@@ -22,7 +22,7 @@ class Program {
       }
     }
   }
-  
+
   public static void Main (string[] args) {
     evaluateRpn();
   }
