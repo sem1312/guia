c923ef4cc5a7d3abb04f456336be407ebb71f3f5
sem1312 <108529990+sem1312@users.noreply.github.com>
2023-11-03 10:49:49 -0300

    Update ej3.cs

--- ej3.cs
+++ ej3.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 
 class Program {
 
-  public static void evaluateRpn (){
-
-    string tamaño0 = Console.ReadLine();
-    int.TryParse(tamaño0, out int tamaño);
+  public Queue<int> filterEven (int tamaño){
 
     Queue<int> myQueue = new Queue<int>();
+    Queue<int> myQueueFiltrado = new Queue<int>();
 
     for(int i = 0; i < tamaño; i++){                 
       string num0 = Console.ReadLine();
@@ -17,13 +15,26 @@ class Program {
     }
 
     foreach(var item in myQueue){
-      if(item % 2 != 0){
-        Console.WriteLine(item);
+      if(item % 2 == 0){
+        myQueueFiltrado.Enqueue(item);
       }
     }
+
+    return myQueueFiltrado;
   }
 
-  public static void Main (string[] args) {
-    evaluateRpn();
+  public static void Main () {
+
+    Program program = new Program();
+
+    string tamaño0 = Console.ReadLine();
+    int.TryParse(tamaño0, out int tamaño);
+
+    Queue<int> resultado = program.filterEven(tamaño);
+
+    foreach (var item in resultado) {
+      Console.WriteLine(item);
+    }
+    
   }
 }
